97fddee3d37764eddbb85b13dea159d3aaf8cecf
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-23 14:19:02 -0300

    Create cont useState and using map to loop through the todos array and render each todo item.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Task 1',
+      isCompleted: true,
+    },
+    {
+      content: 'Task 2',
+      isCompleted: false,
+    },
+    {
+      content: 'Task 3',
+      isCompleted: false,
+    },
+  ]);
+
   return (
     <div className="App">
       <h1 className="title">My to do list</h1>
       <form className="todo-list">
         <ul className="todo-list__ul">
-          <div className="todo">
-            <div className="checkbox"></div>
-            <input type="text" value="Todo one"></input>
-          </div>
+          {todos.map((todo, i) => (
+            <div className="todo">
+              <div className="checkbox"></div>
+              <input type="text" value={todo.content} />
+            </div>
+          ))}
         </ul>
       </form>
     </div>
