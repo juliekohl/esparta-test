cdaf860535a7eb92f572b2ca1efbb118f6dcb5aa
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-23 14:17:13 -0300

    Create HTML.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My to do list</h1>
+      <h1 className="title">My to do list</h1>
+      <form className="todo-list">
+        <ul className="todo-list__ul">
+          <div className="todo">
+            <div className="checkbox"></div>
+            <input type="text" value="Todo one"></input>
+          </div>
+        </ul>
+      </form>
     </div>
   );
 }
