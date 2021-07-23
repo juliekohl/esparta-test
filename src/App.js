884eda9db8c2395be310bdb6b6bba315663f9f2d
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-23 15:43:07 -0300

    Add confirm window.

--- src/App.js
+++ src/App.js
@@ -47,10 +47,12 @@ function App() {
 
   function removeTodoTask(i) {
     if (i === 0 && todos.length === 1) return;
-    setTodos((todos) => todos.splice(0, i).concat(todos.splice(i + 1, todos.length)));
-    setTimeout(() => {
-      document.forms[0].elements[i - 1].focus();
-    }, 0);
+    if (window.confirm('Deseja deletar essa tarefa?')) {
+      setTodos((todos) => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+      setTimeout(() => {
+        document.forms[0].elements[i - 1].focus();
+      }, 0)
+    }
   }
 
   function toggleTodoTaskComplete(index) {
