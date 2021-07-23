e3d8a9c6a3d08e0f7c2eab799141d9e308881179
Julie Kohl <juliana.cochenski@gmail.com>
2021-07-23 15:05:22 -0300

    Create new todo, update and remove.

--- src/App.js
+++ src/App.js
@@ -17,15 +17,56 @@ function App() {
     },
   ]);
 
+  function handlekeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodoTask(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefaul();
+      return removeTodoTask(i);
+    }
+  }
+
+  function createTodoTask(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodoTask(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function removeTodoTask(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos((todos) => todos.splice(0, i).concat(todos.splice(i + 1, todos.length)));
+    setTimeout(() => {
+      document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
   return (
     <div className="App">
-      <h1 className="title">My to do list</h1>
       <form className="todo-list">
+        <h1 className="title">My to do list</h1>
         <ul className="todo-list__ul">
           {todos.map((todo, i) => (
             <div className="todo">
               <div className="checkbox"></div>
-              <input type="text" value={todo.content} />
+              <input
+                type="text"
+                value={todo.content} 
+                onKeyDown={(e) => handlekeyDown(e, i)}
+                onChange={(e) => updateTodoTask(e, i)}
+              />
             </div>
           ))}
         </ul>
